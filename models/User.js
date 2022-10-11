863fe4895f11a2920f3bfe8b458cd804aac2947c
unknown <wiwimarti0693@gmail.com>
2022-10-11 16:36:54 +0200

    add modele User

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+
+const SchemaUser = new mongoose.Schema(
+    {
+            nom : {
+                type: String,
+                trim: true,
+                required : true,
+            },
+            prenom : {
+                type: String,
+                trim: true,
+                required : true,
+            },
+
+            email : {
+                type: String,
+                trim: true,
+                unique : true,
+                required : true,
+                lowercase : true,
+            },
+            tel : {
+                type : Number,
+            },
+            password : {
+                type : string,
+                required : true,
+            }
+
+    },
+    {timestamps : true }
+);
+
+// exportation du model 
+module.exports = mongoose.model("User", SchemaUser)
