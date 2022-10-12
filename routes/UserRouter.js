f76687997fa802314e17ee45af6de498beca4191
hanenetriaa <hanene.triaa@live.fr>
2022-10-12 15:38:35 +0200

    /:id in router getprofilbyid

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -5,7 +5,7 @@ const userController = require("../controllers/UserControllers");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.get("/getProfile", userController.getUserById);
+router.get("/getProfile/:id", userController.getUserById);
 router.post("/logout/:id", userController.logout);
 
 module.exports = router;
