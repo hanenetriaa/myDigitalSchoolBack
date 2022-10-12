87115047a68ad5c328b4bd4bcaa098f2440d6979
unknown <wiwimarti0693@gmail.com>
2022-10-12 15:01:05 +0200

    add package passport

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,11 +1,12 @@
 /** @format */
 
 const router = require("express").Router();
-
 const userController = require("../controllers/UserControllers");
+const passport = require("passport");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.delete("/logout", userController.logout);
+router.delete("/logout", passport.authenticate("jwt", { session: false }), userController.logout );
+
 
 module.exports = router;
